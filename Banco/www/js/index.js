f76d4e5de513054546b5d71ed3d6279cd5283e6a
Tiago <tiago.tiaguinho2013@hotmail.com>
2022-05-22 21:34:26 -0300

    Banco de Dados

--- Banco/www/js/index.js
+++ Banco/www/js/index.js
@@ -1,29 +1,62 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+var app = {
+        
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+    onDeviceReady: function() {
+        document.getElementById("btnInserir").addEventListener("click",app.inserir);
+        document.getElementById("btnListar").addEventListener("click",app.listar);
+        this.receivedEvent('deviceready');
+    },
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        db = window.sqlitePlugin.openDatabase({
+            name: 'my.db',
+            location: 'default',            
+            androidDatabaseProvider: 'system'
+        });
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-}
+        db.transaction(function(tx) {
+            tx.executeSql('CREATE TABLE IF NOT EXISTS usuarios (login, pass)');
+        }, function(error) {
+            console.log('Transaction ERROR: ' + error.message);
+        }, function() {
+            alert('Banco e Tabela usuarios criados com sucesso!!!');
+        });
+    },
+
+    inserir: function(){
+        let login = document.getElementById("txtLogin").value;
+        let pass = document.getElementById("txtPassword").value;
+
+        db.transaction(function(tx) {
+            tx.executeSql('INSERT INTO usuarios VALUES (?,?)', [login, pass]);
+        }, function(error) {
+            alert('Erro durante a transacao com o banco: ' + error.message);
+        }, function() {
+            alert('Insercao realizada com sucesso!!!');
+        });
+    },
+    
+    listar: function(){
+        db.executeSql(
+            'SELECT login AS uLoginName, pass AS uPassword FROM usuarios', [], function(rs) {
+                alert(JSON.stringify(rs));
+                alert(rs.rows.length);
+                let i = 0;
+                for(i = 0; i < rs.rows.length; i++){
+                    alert("item "+i);
+                    let recordItem = rs.rows.item(i);
+                    alert(JSON.stringify(recordItem));
+                }                
+            //alert('Record count (expected to be 2): ' + rs.rows.item(0).uLoginName);
+        }, function(error) {
+            alert('Erro no SELECT: ' + error.message);
+        });
+    }
+};
+
+app.initialize();
